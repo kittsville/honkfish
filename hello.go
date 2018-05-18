002f0c4e85e3256d8d710a3857e8814208b69dc2
Kit <kit.maywood@intenthq.com>
2018-05-18 15:17:38 +0100

    Replace spaces in honks with letter p
    Avoids confusing when debugging + URL issues

--- hello.go
+++ hello.go
@@ -12,21 +12,21 @@ func translate(honks string) string {
 
   /*
   Translation map from honks to the boat's behaviour
-  s       = short honk
-  l       = long honk
-  <space> = gap between honks
+  s = short honk
+  l = long honk
+  p = pause between honks
   */
   honkTranslations["s"] = "I am altering my course to STARBOARD"
   honkTranslations["ss"] = "I am altering my course to PORT"
   honkTranslations["sss"] = "I am going ASTERN"
-  honkTranslations["ssss s"] = "I am turning through 360 degrees to STARBOARD"
-  honkTranslations["ssss ss"] = "I am turning through 360 degrees to PORT"
+  honkTranslations["ssssps"] = "I am turning through 360 degrees to STARBOARD"
+  honkTranslations["sssspss"] = "I am turning through 360 degrees to PORT"
   honkTranslations["sssss"] = "I do not understand your intentions, *keep clear*, I doubt whether you are taking sufficient action to avoid a collision"
   honkTranslations["l"] = "I am about to get underway, enter the fairway or I am approaching a blind bend"
-  honkTranslations["l s s"] = "I am unable to manoeuvre - not under command"
-  honkTranslations["l l s"] = "I intend to overtake you on YOUR STARBOARD side"
-  honkTranslations["l l s s"] = "I intend to overtake you on YOUR PORT side"
-  honkTranslations["l s l s"] = "I agree to be overtaken"
+  honkTranslations["lpsps"] = "I am unable to manoeuvre - not under command"
+  honkTranslations["lplps"] = "I intend to overtake you on YOUR STARBOARD side"
+  honkTranslations["lplpsps"] = "I intend to overtake you on YOUR PORT side"
+  honkTranslations["lpsplps"] = "I agree to be overtaken"
 
   if translation, found := honkTranslations[honks]; found {
     return translation
